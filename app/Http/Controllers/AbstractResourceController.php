a1485509ac29a4a4fc003be91b51272a83920977
Boy Havelaar <boy.havelaar@radancy.com>
2022-05-27 14:15:32 +0200

    plof

--- app/Http/Controllers/AbstractResourceController.php
+++ app/Http/Controllers/AbstractResourceController.php
@@ -4,18 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Contracts\Support\ClassInstantiator;
 use App\Http\Requests\ApiRequest;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 
 abstract class AbstractResourceController extends BaseController
 {
     protected ApiRequest $request;
 
-    public function __construct(private ClassInstantiator $instantiator)
+    public function __construct(private ClassInstantiator $instantiator, private ResponseFactory $responseFactory)
     {
     }
 
     protected function setRequest(string $requestClass = null): void
     {
         $this->request = $this->instantiator->instantiate($requestClass ?: ApiRequest::class);
+    }
+
+    protected function returnResponse(array $data = [], int $status = JsonResponse::HTTP_OK): JsonResponse
+    {
+        return $this->responseFactory->json($data, $status);
     }
 }
--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,20 +6,13 @@ namespace App\Http\Controllers;
 
 use App\Contracts\Support\ClassInstantiator;
 use App\Http\Requests\PaginatedApiRequest;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Response;
 
-final class MainController extends AbstractApiController
+final class MainController extends AbstractResourceController
 {
     public function __construct(ClassInstantiator $instantiator)
     {
         parent::__construct($instantiator);
 
         $this->setRequest(PaginatedApiRequest::class);
-    }
-
-    public function index(): JsonResponse
-    {
-        return new JsonResponse(['page' => $this->request->getPage()], Response::HTTP_OK);
     }
 }
