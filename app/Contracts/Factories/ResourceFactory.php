283d102d76546ce8bc8d6d4c4e7c18e4eb4c3231
Boy Havelaar <boy.havelaar@radancy.com>
2022-05-27 16:38:23 +0200

    iterface

--- app/Contracts/Factories/ResourceFactory.php
+++ app/Contracts/Factories/ResourceFactory.php
@@ -6,14 +6,18 @@ namespace App\Contracts\Factories;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
 use Illuminate\Support\Collection;
 
 interface ResourceFactory
 {
-    public function createForModel(Model $model, string $resourceClass): JsonResource;
+    public function createForModel(Model $model, string $resourceClass = JsonResource::class): JsonResource;
 
     /**
      * @param Collection<Model>
      */
-    public function createForModelCollection(Collection $models, string $resourceCollectionClass): JsonResource;
+    public function createForModelCollection(
+        Collection $models,
+        string $resourceCollectionClass = ResourceCollection::class
+    ): JsonResource;
 }
